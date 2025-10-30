fd14430b463614a31fd6a73f6fc30eb7926fbf13
Panshul Sharma <panshulsharma93@gmail.com>
2025-10-30 17:58:50 +0000

    I AM NOT ABLE TO DO BECAUSE WE ARE FACING THE SAME ERROR WHY THE SAME ER

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
